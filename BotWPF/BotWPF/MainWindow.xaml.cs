e8314f87579d52479321d9969a74c73a7aa38248
Slypi1 <anastasuasluckix94@gmail.com>
2022-06-08 00:31:10 +0700

    Добавьте файлы проекта.

---
+++ BotWPF/BotWPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Telegram.Bot.Types;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace BotWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TelegramMessageClient client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+         
+
+            client = new TelegramMessageClient(this);
+
+            logList.ItemsSource = client.BotMessageLog;
+        }
+
+        private void btnMsgSendClick(object sender, RoutedEventArgs e)
+        {
+            client.SendMessage(txtMsgSend.Text, TargetSend.Text);
+        }
+        private void SaveJsonClick (object sender,RoutedEventArgs e)
+            {
+             client.SaveJson();
+
+            }
+        private void CloseForm (object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
---
+++ BotWPF/BotWPF/TelegramMessageClient.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using System.Threading;
+
+namespace BotWPF
+
+
+
+{
+    class TelegramMessageClient
+
+    {
+         MainWindow w;
+
+        private TelegramBotClient bot;
+        public  ObservableCollection<MessageLog> BotMessageLog { get; set; }
+
+        
+       
+        public void MessageListener ( Message message)
+        {
+            string text=$"{DateTime.Now.ToLongTimeString()}:{message.Chat.FirstName}{message.Chat.Id} {message.Text}";
+            Debug.WriteLine($"{text} TypeMessage: {message.Type.ToString()}");
+           
+
+            if (message.Text == null) return;
+
+            var messageText = message.Text;
+
+           
+            w.Dispatcher.Invoke(() =>
+            {
+                BotMessageLog.Add(
+                new MessageLog(
+                    DateTime.Now.ToLongTimeString(), messageText,message.Chat.FirstName, message.Chat.Id));
+            });
+
+            
+            
+        }
+        public void SaveJson()
+        {
+       
+          
+            string serialized = JsonConvert.SerializeObject(BotMessageLog);
+       
+            if (System.IO.File.Exists(@"savemessage.json"))
+                  System.IO.File.Create(@"savemessage.json").Close();
+            System.IO.File.AppendAllText (@"savemessage.json", serialized);
+
+            
+
+        }
+
+        public TelegramMessageClient(MainWindow W, string PathToken = @"C:\Dowload\token.txt")
+        {
+            this.BotMessageLog = new ObservableCollection<MessageLog>();
+            this.w = W;
+
+            bot = new TelegramBotClient(System.IO.File.ReadAllText(PathToken));
+            var cts = new CancellationTokenSource();
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }
+            };
+            bot.StartReceiving(
+            HandleUpdateAsync,
+            HandleErrorAsync,
+            receiverOptions,
+            cancellationToken: cts.Token
+            );
+           
+
+          
+        }
+        public void SendMessage(string Text, string Id)
+        {
+            long id = Convert.ToInt64(Id);
+            bot.SendTextMessageAsync(id, Text);
+        }
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+           
+            if (update.Type == UpdateType.Message && update?.Message?.Text != null)
+
+            {
+                  
+                 await HandleMessage(botClient, update.Message);
+                  
+                return;
+            }
+
+        }
+       async Task HandleMessage(ITelegramBotClient botClient, Message message)
+        {
+
+            if (message.Text == "/start")
+            
+            {
+                
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Привествую, я готов перекинуть твои данные)", replyToMessageId: message.MessageId);
+                return;
+            }
+
+
+            if (message.Text == "Привет")
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Привет, привет давай кидай чего нибудь)", replyToMessageId: message.MessageId);
+
+             MessageListener(message);
+            
+
+        }
+        static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+          
+            return Task.CompletedTask;
+        }
+
+    }
+}
